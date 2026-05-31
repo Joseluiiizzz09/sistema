7a4d3ecc6c4ebe094c5f5139683dd4d53773d696
joseluiiizzz <1630903@senati.pe>
2026-05-30 19:33:16 -0500

    actualización frontend

---
+++ js/ubigeo.js
@@ -0,0 +1,580 @@
+const UBIGEO = 
+{
+  "AMAZONAS": {
+    "BAGUA": [
+      "BAGUA"
+    ],
+    "CHACHAPOYAS": [
+      "CHACHAPOYAS"
+    ],
+    "UTCUBAMBA": [
+      "BAGUA GRANDE"
+    ]
+  },
+  "ANCASH": {
+    "CASMA": [
+      "CASMA"
+    ],
+    "HUARAZ": [
+      "HUARAZ",
+      "INDEPENDENCIA"
+    ],
+    "HUARMEY": [
+      "HUARMEY"
+    ],
+    "HUAYLAS": [
+      "CARAZ"
+    ],
+    "SANTA": [
+      "CHIMBOTE",
+      "COISHCO",
+      "NUEVO CHIMBOTE",
+      "SANTA"
+    ],
+    "YUNGAY": [
+      "YUNGAY"
+    ]
+  },
+  "APURIMAC": {
+    "ABANCAY": [
+      "ABANCAY",
+      "TAMBURCO"
+    ],
+    "ANDAHUAYLAS": [
+      "ANDAHUAYLAS"
+    ],
+    "CHINCHEROS": [
+      "ANCO-HUALLO"
+    ],
+    "COTABAMBAS": [
+      "CHALLHUAHUACHO"
+    ]
+  },
+  "AREQUIPA": {
+    "AREQUIPA": [
+      "ALTO SELVA ALEGRE",
+      "AREQUIPA",
+      "CAYMA",
+      "CERRO COLORADO",
+      "CHARACATO",
+      "JACOBO HUNTER",
+      "JOSE LUIS BUSTAMANTE Y RIVERO",
+      "LA JOYA",
+      "MARIANO MELGAR",
+      "MIRAFLORES",
+      "PAUCARPATA",
+      "SABANDIA",
+      "SACHACA",
+      "SOCABAYA",
+      "UCHUMAYO",
+      "YANAHUARA",
+      "YURA"
+    ],
+    "CAMANA": [
+      "CAMANA",
+      "MARIANO NICOLAS VALCARCEL",
+      "NICOLAS DE PIEROLA",
+      "SAMUEL PASTOR"
+    ],
+    "CARAVELI": [
+      "CHALA"
+    ],
+    "CAYLLOMA": [
+      "MAJES"
+    ],
+    "ISLAY": [
+      "MEJIA",
+      "MOLLENDO"
+    ]
+  },
+  "AYACUCHO": {
+    "HUAMANGA": [
+      "ANDRES AVELINO CACERES DORREGARAY",
+      "AYACUCHO",
+      "CARMEN ALTO",
+      "JESUS NAZARENO",
+      "SAN JUAN BAUTISTA"
+    ],
+    "HUANTA": [
+      "HUANTA"
+    ],
+    "LA MAR": [
+      "AYNA"
+    ],
+    "LUCANAS": [
+      "PUQUIO"
+    ],
+    "PARINACOCHAS": [
+      "CORACORA"
+    ]
+  },
+  "CAJAMARCA": {
+    "CAJABAMBA": [
+      "CAJABAMBA"
+    ],
+    "CAJAMARCA": [
+      "CAJAMARCA",
+      "LOS BAÑOS DEL INCA"
+    ],
+    "CELENDIN": [
+      "CELENDIN"
+    ],
+    "CHOTA": [
+      "CHOTA"
+    ],
+    "CUTERVO": [
+      "CUTERVO"
+    ],
+    "HUALGAYOC": [
+      "BAMBAMARCA"
+    ],
+    "JAEN": [
+      "JAEN"
+    ],
+    "SAN IGNACIO": [
+      "SAN IGNACIO"
+    ]
+  },
+  "CUSCO": {
+    "CALCA": [
+      "CALCA"
+    ],
+    "CANCHIS": [
+      "SICUANI"
+    ],
+    "CUSCO": [
+      "CUSCO",
+      "SAN JERONIMO",
+      "SAN SEBASTIAN",
+      "SANTIAGO",
+      "WANCHAQ"
+    ],
+    "ESPINAR": [
+      "ESPINAR"
+    ],
+    "LA CONVENCION": [
+      "KIMBIRI",
+      "PICHARI",
+      "SANTA ANA"
+    ],
+    "QUISPICANCHI": [
+      "URCOS"
+    ],
+    "URUBAMBA": [
+      "URUBAMBA"
+    ]
+  },
+  "HUANCAVELICA": {
+    "ANGARAES": [
+      "LIRCAY"
+    ],
+    "HUANCAVELICA": [
+      "HUANCAVELICA"
+    ],
+    "TAYACAJA": [
+      "DANIEL HERNANDEZ",
+      "PAMPAS"
+    ]
+  },
+  "HUANUCO": {
+    "AMBO": [
+      "AMBO"
+    ],
+    "HUANUCO": [
+      "AMARILIS",
+      "HUANUCO",
+      "PILLCO MARCA"
+    ],
+    "LEONCIO PRADO": [
+      "CASTILLO GRANDE",
+      "JOSE CRESPO Y CASTILLO",
+      "MARIANO DAMASO BERAUN",
+      "RUPA-RUPA"
+    ],
+    "PUERTO INCA": [
+      "PUERTO INCA"
+    ]
+  },
+  "ICA": {
+    "CHINCHA": [
+      "CHINCHA ALTA",
+      "GROCIO PRADO",
+      "PUEBLO NUEVO",
+      "SUNAMPE"
+    ],
+    "ICA": [
+      "ICA",
+      "LA TINGUIÑA",
+      "PARCONA",
+      "SALAS",
+      "SAN JUAN BAUTISTA",
+      "SUBTANJALLA"
+    ],
+    "NAZCA": [
+      "MARCONA",
+      "NAZCA",
+      "VISTA ALEGRE"
+    ],
+    "PISCO": [
+      "PARACAS",
+      "PISCO",
+      "TUPAC AMARU INCA"
+    ]
+  },
+  "JUNIN": {
+    "CHANCHAMAYO": [
+      "CHANCHAMAYO",
+      "PERENE",
+      "PICHANAQUI",
+      "SAN RAMON"
+    ],
+    "CHUPACA": [
+      "CHUPACA"
+    ],
+    "HUANCAYO": [
+      "CHILCA",
+      "EL TAMBO",
+      "HUANCAYO",
+      "PILCOMAYO"
+    ],
+    "JAUJA": [
+      "JAUJA",
+      "SAUSA",
+      "YAUYOS"
+    ],
+    "SATIPO": [
+      "MAZAMARI",
+      "PANGOA",
+      "SATIPO"
+    ],
+    "TARMA": [
+      "TARMA"
+    ],
+    "YAULI": [
+      "LA OROYA",
+      "SANTA ROSA DE SACCO"
+    ]
+  },
+  "LA LIBERTAD": {
+    "ASCOPE": [
+      "CASA GRANDE"
+    ],
+    "CHEPEN": [
+      "CHEPEN"
+    ],
+    "OTUZCO": [
+      "OTUZCO"
+    ],
+    "PACASMAYO": [
+      "PACASMAYO",
+      "SAN PEDRO DE LLOC"
+    ],
+    "SANCHEZ CARRION": [
+      "HUAMACHUCO"
+    ],
+    "TRUJILLO": [
+      "EL PORVENIR",
+      "FLORENCIA DE MORA",
+      "HUANCHACO",
+      "LA ESPERANZA",
+      "LAREDO",
+      "MOCHE",
+      "SALAVERRY",
+      "TRUJILLO",
+      "VICTOR LARCO HERRERA"
+    ],
+    "VIRU": [
+      "CHAO",
+      "VIRU"
+    ]
+  },
+  "LAMBAYEQUE": {
+    "CHICLAYO": [
+      "CHICLAYO",
+      "ETEN",
+      "JOSE LEONARDO ORTIZ",
+      "LA VICTORIA",
+      "MONSEFU",
+      "PICSI",
+      "PIMENTEL",
+      "POMALCA",
+      "REQUE",
+      "TUMAN"
+    ],
+    "FERREÑAFE": [
+      "FERREÑAFE",
+      "PUEBLO NUEVO"
+    ],
+    "LAMBAYEQUE": [
+      "LAMBAYEQUE",
+      "MOTUPE",
+      "OLMOS",
+      "TUCUME"
+    ]
+  },
+  "LIMA": {
+    "BARRANCA": [
+      "BARRANCA",
+      "PARAMONGA",
+      "SUPE",
+      "SUPE PUERTO"
+    ],
+    "CALLAO": [
+      "BELLAVISTA",
+      "CALLAO",
+      "CARMEN DE LA LEGUA REYNOSO",
+      "LA PERLA",
+      "LA PUNTA",
+      "MI PERU",
+      "VENTANILLA"
+    ],
+    "CAÑETE": [
+      "CHILCA",
+      "IMPERIAL",
+      "MALA",
+      "SAN VICENTE DE CAÑETE"
+    ],
+    "HUARAL": [
+      "CHANCAY",
+      "HUARAL"
+    ],
+    "HUAURA": [
+      "CALETA DE CARQUIN",
+      "HUACHO",
+      "HUALMAY",
+      "HUAURA",
+      "SANTA MARIA"
+    ],
+    "LIMA": [
+      "ANCON",
+      "ATE",
+      "BARRANCO",
+      "BREÑA",
+      "CARABAYLLO",
+      "CERCADO DE LIMA",
+      "CHACLACAYO",
+      "CHORRILLOS",
+      "CIENEGUILLA",
+      "COMAS",
+      "EL AGUSTINO",
+      "INDEPENDENCIA",
+      "JESUS MARIA",
+      "LA MOLINA",
+      "LA VICTORIA",
+      "LIMA",
+      "LINCE",
+      "LOS OLIVOS",
+      "LURIGANCHO",
+      "LURIN",
+      "MAGDALENA DEL MAR",
+      "MIRAFLORES",
+      "PACHACAMAC",
+      "PUCUSANA",
+      "PUEBLO LIBRE",
+      "PUENTE PIEDRA",
+      "PUNTA HERMOSA",
+      "RIMAC",
+      "SAN BARTOLO",
+      "SAN BORJA",
+      "SAN ISIDRO",
+      "SAN JUAN DE LURIGANCHO",
+      "SAN JUAN DE MIRAFLORES",
+      "SAN LUIS",
+      "SAN MARTIN DE PORRES",
+      "SAN MIGUEL",
+      "SANTA ANITA",
+      "SANTA MARIA DEL MAR",
+      "SANTA ROSA",
+      "SANTIAGO DE SURCO",
+      "SURQUILLO",
+      "VILLA EL SALVADOR",
+      "VILLA MARIA DEL TRIUNFO"
+    ]
+  },
+  "LORETO": {
+    "ALTO AMAZONAS": [
+      "YURIMAGUAS"
+    ]
+  },
+  "MADRE DE DIOS": {
+    "MANU": [
+      "HUEPETUHE"
+    ],
+    "TAMBOPATA": [
+      "INAMBARI",
+      "TAMBOPATA"
+    ]
+  },
+  "MOQUEGUA": {
+    "ILO": [
+      "ILO",
+      "PACOCHA"
+    ],
+    "MARISCAL NIETO": [
+      "MOQUEGUA",
+      "SAMEGUA"
+    ]
+  },
+  "PASCO": {
+    "OXAPAMPA": [
+      "CHONTABAMBA",
+      "OXAPAMPA",
+      "PUERTO BERMUDEZ",
+      "VILLA RICA"
+    ],
+    "PASCO": [
+      "CHAUPIMARCA",
+      "YANACANCHA"
+    ]
+  },
+  "PIURA": {
+    "HUANCABAMBA": [
+      "HUANCABAMBA"
+    ],
+    "MORROPON": [
+      "CHULUCANAS"
+    ],
+    "PAITA": [
+      "PAITA"
+    ],
+    "PIURA": [
+      "CASTILLA",
+      "CATACAOS",
+      "LA UNION",
+      "PIURA",
+      "TAMBO GRANDE",
+      "VEINTISEIS DE OCTUBRE"
+    ],
+    "SECHURA": [
+      "SECHURA"
+    ],
+    "SULLANA": [
+      "BELLAVISTA",
+      "SULLANA"
+    ],
+    "TALARA": [
+      "LOS ORGANOS",
+      "MANCORA",
+      "PARIÑAS"
+    ]
+  },
+  "PUNO": {
+    "AZANGARO": [
+      "AZANGARO"
+    ],
+    "CARABAYA": [
+      "MACUSANI"
+    ],
+    "CHUCUITO": [
+      "DESAGUADERO",
+      "JULI"
+    ],
+    "EL COLLAO": [
+      "ILAVE"
+    ],
+    "HUANCANE": [
+      "HUANCANE"
+    ],
+    "MELGAR": [
+      "AYAVIRI"
+    ],
+    "PUNO": [
+      "ACORA",
+      "PUNO"
+    ],
+    "SAN ANTONIO DE PUTINA": [
+      "ANANEA"
+    ],
+    "SAN ROMAN": [
+      "JULIACA",
+      "SAN MIGUEL"
+    ],
+    "SANDIA": [
+      "SANDIA"
+    ],
+    "YUNGUYO": [
+      "YUNGUYO"
+    ]
+  },
+  "SAN MARTIN": {
+    "BELLAVISTA": [
+      "BELLAVISTA"
+    ],
+    "MARISCAL CACERES": [
+      "JUANJUI"
+    ],
+    "MOYOBAMBA": [
+      "MOYOBAMBA"
+    ],
+    "RIOJA": [
+      "NUEVA CAJAMARCA",
+      "RIOJA"
+    ],
+    "SAN MARTIN": [
+      "CACATACHI",
+      "LA BANDA DE SHILCAYO",
+      "MORALES",
+      "TARAPOTO"
+    ],
+    "TOCACHE": [
+      "TOCACHE"
+    ]
+  },
+  "TACNA": {
+    "TACNA": [
+      "ALTO DE LA ALIANZA",
+      "CIUDAD NUEVA",
+      "CORONEL GREGORIO ALBARRACIN LANCHIPA",
+      "POCOLLAY",
+      "TACNA"
+    ]
+  },
+  "TUMBES": {
+    "CONTRALMIRANTE VILLAR": [
+      "ZORRITOS"
+    ],
+    "TUMBES": [
+      "CORRALES",
+      "TUMBES"
+    ],
+    "ZARUMILLA": [
+      "ZARUMILLA"
+    ]
+  },
+  "UCAYALI": {
+    "CORONEL PORTILLO": [
+      "CALLERIA",
+      "MANANTAY",
+      "YARINACOCHA"
+    ],
+    "PADRE ABAD": [
+      "PADRE ABAD"
+    ]
+  }
+}
+;
+
+function actualizarProvincias() {
+  const dep  = document.getElementById('nv_dpto')?.value || '';
+  const selP = document.getElementById('nv_prov');
+  const selD = document.getElementById('nv_dist');
+  if (!selP) return;
+  selP.innerHTML = '<option value="">Seleccionar provincia</option>';
+  if (selD) selD.innerHTML = '<option value="">Seleccionar distrito</option>';
+  if (!dep || !UBIGEO[dep]) return;
+  Object.keys(UBIGEO[dep]).forEach(p => {
+    selP.innerHTML += '<option value="' + p + '">' + p + '</option>';
+  });
+}
+
+function actualizarDistritos() {
+  const dep  = document.getElementById('nv_dpto')?.value || '';
+  const prov = document.getElementById('nv_prov')?.value || '';
+  const selD = document.getElementById('nv_dist');
+  if (!selD) return;
+  selD.innerHTML = '<option value="">Seleccionar distrito</option>';
+  if (!dep || !prov || !UBIGEO[dep]?.[prov]) return;
+  UBIGEO[dep][prov].forEach(d => {
+    selD.innerHTML += '<option value="' + d + '">' + d + '</option>';
+  });
+}
