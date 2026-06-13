c8b68d7603128b22fc41160bb35f96eb6d0518f1
joseluiiizzz <1630903@senati.pe>
2026-06-13 18:35:44 -0500

    prod: config.js actualizado para deploy

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-// config.js — detecta entorno automáticamente
-window.NC_API = (location.hostname === 'localhost' || location.hostname === '127.0.0.1')
+﻿window.NC_API = (location.hostname === 'localhost' || location.hostname === '127.0.0.1')
   ? 'http://localhost:3000'
-  : 'https://TU-BACKEND.up.railway.app'; // se cambia al hacer deploy
+  : '';
